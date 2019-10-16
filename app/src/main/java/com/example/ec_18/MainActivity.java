be3ba2bfd9deeacdd9ffdb692c9fd0dbc535e92c
luigiascione <luigi__96@hotmail.it>
2019-10-16 15:09:57 +0200

    15 ottobre 19:43

--- app/src/main/java/com/example/ec_18/MainActivity.java
+++ app/src/main/java/com/example/ec_18/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        BottomNavigationView navView = findViewById(R.id.Luigi2);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
