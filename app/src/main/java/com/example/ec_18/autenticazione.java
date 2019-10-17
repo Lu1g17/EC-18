34fd55144217a488ea0150cec17f90716a972b91
luigiascione <luigi__96@hotmail.it>
2019-10-17 16:46:33 +0200

    17 ottobre 16:46

---
+++ app/src/main/java/com/example/ec_18/autenticazione.java
@@ -0,0 +1,24 @@
+package com.example.ec_18;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class autenticazione extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.autenticazione);
+
+
+    }
+
+    public void show(final Context packageContext) {
+        Intent i = new Intent(packageContext, autenticazione.class);
+        packageContext.startActivity(i);
+    }
+
+}
---
+++ app/src/main/java/com/example/ec_18/primapagina.java
@@ -0,0 +1,28 @@
+package com.example.ec_18;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class primapagina extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.primapagina); //per dare la relazione tra classe java e layout
+
+        Button login = findViewById(R.id.buttonLogin);  //dichiaro una variabile di tipo bottone e gli assegno il bottone creato nel layout
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new autenticazione().show(primapagina.this);
+            }
+        });
+    }
+
+
+
+}
